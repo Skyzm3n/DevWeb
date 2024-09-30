496ad848cb317070bfb6ba0888e81a9066448e01
Skyzm3n <145280407+Skyzm3n@users.noreply.github.com>
2024-10-01 08:50:04 +1100

    Exercice 2.7 loglevel

--- DERRIEN_Benoit_CC3/server-express.mjs
+++ DERRIEN_Benoit_CC3/server-express.mjs
@@ -1,13 +1,14 @@
 import express from "express";
 import morgan from "morgan";
-
 import createError from "http-errors";
-
+import logger from "loglevel";
 
 const host = "localhost";
 const port = 8000;
 
 const app = express();
+
+logger.setLevel(logger.levels.WARN);
 
 if (app.get("env") === "development") app.use(morgan("dev"));
 
@@ -18,21 +19,23 @@ app.set("view engine", "ejs");
 app.get("/random/:nb", async function (request, response, next) {
   const length = Number.parseInt(request.params.nb, 10);
   if (Number.isNaN(length)) {
+    logger.warn(`Nombre incorrect passé en parametre: ${request.params.nb}`);
     return next(createError(400, "Nombre incorrect passé en parametre"));
   }
-
+  
   const numbers = Array.from({ length }, () => Math.floor(100 * Math.random()));
   const welcome = "Voici la liste des nombres genere aleatoirement :";
+  logger.debug(`Nombres generes aleatoirement: ${numbers}`);
   return response.render("random", { numbers, welcome });
 });
 
 app.use((request, response, next) => {
-  concole.debug(`default route handler : ${request.url}`);
-  return next(createError(404));
+  logger.debug(`default route handler: ${request.url}`);
+  return next(createError(404, "Page not found"));
 });
 
 app.use((error, _request, response, _next) => {
-  concole.debug(`default error handler: ${error}`);
+  logger.error(`default error handler: ${error}`);
   const status = error.status ?? 500;
   const stack = app.get("env") === "development" ? error.stack : "";
   const result = { code: status, message: error.message, stack };
@@ -42,9 +45,9 @@ app.use((error, _request, response, _next) => {
 const server = app.listen(port, host);
 
 server.on("listening", () =>
-  console.info(
+  logger.info(
     `HTTP listening on http://${server.address().address}:${server.address().port} with mode '${process.env.NODE_ENV}'`,
   ),
 );
 
-console.info(`File ${import.meta.url} executed.`);
+logger.info(`File ${import.meta.url} executed.`);
